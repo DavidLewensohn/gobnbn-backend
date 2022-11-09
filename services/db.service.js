273e0f4066c4ea0dd5e62bbcc744cc7749e527c7
DavidLewensohn <davidlewensohn@gmail.com>
2022-11-09 16:42:15 +0200

    render deploy

--- services/db.service.js
+++ services/db.service.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient
 
 const config = require('../config')
+const { dbURL } = require('../config/prod')
 
 module.exports = {
     getCollection
@@ -27,6 +28,7 @@ async function connect() {
     if (dbConn) return dbConn
     try {
         const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log('dbURL:',config.dbURL)///
         const db = client.db(dbName)
         dbConn = db
         return db
